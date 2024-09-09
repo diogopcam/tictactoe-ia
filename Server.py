453f897a298c8c98f768a8f60c19008d7a8be183
Diogo <134295616+diogopcam@users.noreply.github.com>
2024-09-09 20:28:02 -0300

    Testando requisições com parâmetros.

--- Server.py
+++ Server.py
@@ -1,11 +1,14 @@
-from flask import Flask, jsonify
-
-app = Flask(__name__)
+from flask import Flask, jsonify, request
 
 # Para testar se o servidor tá funcionando
 @app.route('/ping', methods=['GET'])
 def ping():
     return jsonify({'message': 'Servidor está funcionando!'})
+
+@app.route('/verifyState', methods=['GET'])
+def verifyState():
+    board = request.args.get('board')
+    return jsonify({'status': board})
 
 if __name__ == '__main__':
     app.run(debug=True)
