21f99f40c5627ba81f3f24b4fd1ac5d294aacb10
Diogo <diogpcam@gmail.com>
2024-09-12 21:59:43 -0300

    Instalacao do cors e ajustes realizados.

--- app.py
+++ app.py
@@ -1,16 +1,19 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, Blueprint
+from flask_cors import CORS
 
 app = Flask('__name__')
+CORS(app)  # Permite todas as origens por padrão
 
 # Para testar se o servidor tá funcionando
 @app.route('/ping', methods=['GET'])
 def ping():
     return jsonify({'message': 'Servidor está funcionando!'})
 
-@app.route('/verifyState', methods=['GET'])
-def verifyState():
-    board = request.args.get('board')
-    return jsonify({'status': board})
+@app.route('/verifyState', methods=['POST'])
+def verify_state():
+    data = request.json
+    app.logger.info('Recebido dados: %s', data)  # Mensagem de log personalizada
+    return jsonify({"message": "Dados recebidos com sucesso!"})
 
 if __name__ == '__main__':
     app.run(debug=True)
